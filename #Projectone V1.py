87ea801f3dc13800faf81a1e1099752f2570128c
Dani_Martinez <danijakemez@gmail.com>
2024-09-29 20:49:08 -0400

    Update #Projectone V2.py

--- #Projectone V1.py
+++ #Projectone V1.py
@@ -1,6 +1,7 @@
-#Projectone V1
-name="Danilo"
+#Projectone V2
+name="Danilo Martinez"
 age=18
 classNUM="ET574"
 print("Hello, this it version one")
 print(f" my name: {name}\n my age:{age}\n my class:{classNUM}")
+print('This is version 2')
